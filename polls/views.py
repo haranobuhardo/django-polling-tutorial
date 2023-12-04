081cae918a64a1a860c2aa11fbb89ae1a1179c89
unknown <Administrator@PCI-3300002110.id.pouchen.com>
2023-12-04 09:47:13 +0700

    modified polls' views and urls

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_id for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
